9c9f9f4020e80840d1fba64eaf565f765a6ca175
Munzir Mukhtar Almahdi /Dev/IT/CTC/KN <momukhtar@ctcdom.ctcgroupltd.com>
2024-03-05 13:30:32 +0200

    lesson 8

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from "react";
-// import { render } from "react-dom";
+// import { Router, Link } from "@gatsbyjs/reach-router";
 import { createRoot } from 'react-dom/client';
 import SearchParams from "./SearchParams";
- 
+import { StrictMode } from 'react';
+import Details from "./Details";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
 const App = () => {
 
     return (
-
-      <div>
-        <h1 id="something-important">Adopt Me!</h1>
-        <SearchParams />
-      </div>
-      
+        <div>
+          <header>
+            <Link to="/">
+              Adopt Me!
+            </Link>
+          </header>
+          {/* <link to="/">
+            <h1 id="something-important">Adopt Me!</h1>
+          </link> */}
+          <Routes>
+            <Route path="/" element={<SearchParams />} />
+            <Route path="/details/:id" element={<Details />} />
+            
+          </Routes>
+        </div>
     );
 
 };
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
-
-// render(<App />, document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,36 +1,46 @@
 import React, {useState, useEffect} from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
 
 const SearchParams = () => {
 
     const [location, setLocation] = useState("Seattle, WA");
     const [breeds, setBreeds] = useState([]);
-    const [animal, AnimalDropdown] = useDropdown("animal", "dog", ANIMALS);
-    const [breed, BreedDropdown, setBreed] = useDropdown("breed", "", breeds);
+    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+    const [pets, setPets] = useState([]);
+
+    async function requestPets(){
+
+        const { animals } = await pet.animals({
+            location,
+            breed,
+            type: animal
+        });
+
+        setPets(animals || []);
+
+    }
 
     useEffect(() => {
-        console.log(pet);
-        // console.log(ANIMALS);
-        // console.log(pet.breeds("dog"));
         setBreeds([]);
         setBreed("");
-
-        setTimeout(() => {
-            console.log("nigga");
-            setBreeds(ANIMALS);
-        }, 500);
         
-        // pet.breeds(animal).then(({ breeds }) => {
-        //     const breedString = breeds.map( ({name}) => name);
-        //     setBreeds(breedString);
-        // }, console.error);
+        pet.breeds(animal).then(({ breeds }) => {
+            const breedString = breeds.map( ({name}) => name);
+            setBreeds(breedString);
+        }, console.error);
+
     }, [animal, setBreed, setBreeds])
 
     return (
         <div className="search-params">
             <h1>{location}</h1>
-            <form>
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                requestPets();
+            }}>
                 <label htmlFor="location">
                     location
                     <input id="location" value={location} placeholder="Location" onChange={event => setLocation(event.target.value)}/>
@@ -41,6 +51,9 @@ const SearchParams = () => {
 
                 <button>Submit</button>
             </form>
+
+            <Results pets={pets} />
+        
         </div>
     );
 
